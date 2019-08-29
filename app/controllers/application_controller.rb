1cd9197a425b7a978f20ddd1f85cb781cdc5da1b
Meghan Stang <mstang15@gmail.com>
2019-08-29 10:29:21 -0600

    Add helper method for current_user_merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :cart,
-                :current_user
+                :current_user,
+                :current_user_merchant
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
   def current_user
-    
+
+  end
+
+  def current_user_merchant
+      # current_user.merchant_id
   end
 
 end
