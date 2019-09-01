0460fed3a14f3369fe41a1a921ab81d8b795e175
Meghan Stang <mstang15@gmail.com>
2019-09-01 11:27:25 -0600

    Add create method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,20 @@ class UsersController <ApplicationController
 
   end
 
+  def create
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "#{user.name}, you are now registered and logged in."
+      redirect_to profile_path
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
 
   def check_for_user
     render file: 'public/404', status: 404 unless current_user?
