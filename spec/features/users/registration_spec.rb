e301ff1139a3d81d8ca13956d219b42a61e2b6ff
Meghan Stang <mstang15@gmail.com>
2019-09-01 11:27:17 -0600

    Add test for creating a user given all fields are filled in

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -1,18 +1,4 @@
-# User Story 10, User Registration
-#
-# As a visitor
-# When I click on the 'register' link in the nav bar
-# Then I am on the user registration page ('/register')
-# And I see a form where I input the following data:
-# - my name
-# - my street address
-# - my city
-# - my state
-# - my zip code
-# - my email address
-# - my preferred password
-# - a confirmation field for my password
-#
+
 # When I fill in this form completely,
 # And with a unique email address not already in the system
 # My details are saved in the database
@@ -25,10 +11,6 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
   describe 'when i click on register in the nav bar' do
     it 'I am on the user registration page and I can see a form' do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
-      allow_any_instance_of(ApplicationController).to receive(:current_admin?).and_return(false)
-      allow_any_instance_of(ApplicationController).to receive(:current_user_merchant?).and_return(false)
-
       visit '/'
       click_on "Register"
 
@@ -42,6 +24,31 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_field("Email")
       expect(page).to have_field("Password")
       expect(page).to have_field("Password confirmation")
+    end
+
+    it 'I am logged in and taken to profile page where I can see a flash message' do
+
+      visit '/register'
+
+      fill_in "Name", with: "Mike"
+      fill_in "Address", with: "123 Dao St."
+      fill_in "City", with: "Denver"
+      fill_in "State", with: "CO"
+      fill_in "Zip", with: 80203
+      fill_in "Email", with: "mdao@gmail.com"
+      fill_in "Password", with: "AthenaIsTheBest"
+      fill_in "Password confirmation", with: "AthenaIsTheBest"
+
+      click_button "Register"
+
+      new_user = User.last
+
+      expect(current_path).to eq('/profile')
+      expect(page).to have_content("Mike, you are now registered and logged in.")
+    end
+
+    it 'I can only create user if i have a unique email' do
+
     end
   end
 end
