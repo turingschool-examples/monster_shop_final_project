cc43a16fae66acc69251dc1d1a9f80ab840a30c9
Meghan Stang <mstang15@gmail.com>
2019-09-05 20:08:11 -0600

    Add instance method for counting all items in an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,8 +6,12 @@ class Order <ApplicationRecord
   belongs_to :user
 
   enum status: ['pending', 'packaged', 'shipped', 'cancelled']
-  
+
   def grandtotal
     item_orders.sum('price * quantity')
+  end
+
+  def count_of_items
+    item_orders.sum(:quantity)
   end
 end
