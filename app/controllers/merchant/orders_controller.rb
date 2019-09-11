8eaeb3f73676060471cf70db144ca9fbd342be9e
Meghan Stang <mstang15@gmail.com>
2019-09-10 20:17:18 -0600

    Add merchant variable in order show action

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -2,5 +2,6 @@ class Merchant::OrdersController <Merchant::BaseController
   def show
     @order = Order.find(params[:id])
     @user = @order.user
+    @merchant = Merchant.find(@user.merchant_id)
   end
 end
